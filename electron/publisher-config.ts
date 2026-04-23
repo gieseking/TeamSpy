f4450ccf4da1fbe79ad750a83a331541819171b2
ebgieseking <egieseking@endpointclinical.com>
2026-04-22 17:55:37 -0700

    Configure TeamSpy Entra app registration

--- electron/publisher-config.ts
+++ electron/publisher-config.ts
@@ -1,3 +1,3 @@
 // Set this once for your distributed builds. Tenant resolution is handled by the user's login.
-export const PUBLISHER_CLIENT_ID = ''
+export const PUBLISHER_CLIENT_ID = '845a127e-8a0f-430b-8d5d-8d0b2c28497b'
 export const AUTHORITY_TENANT = 'organizations'
